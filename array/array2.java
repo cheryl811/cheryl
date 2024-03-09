bf6561f09480bacea272e7fe63574d546a5f3f88
cheryl811 <cheryllalparakal@gmail.com>
2024-03-09 13:48:46 +0530

    3nd commit

--- array/array2.java
+++ array/array2.java
@@ -5,7 +5,7 @@ class BubbleSort {
         for (int i = 0; i < n - 1; i++) 
             for (int j = 0; j < n - i - 1; j++) 
                 if (arr[j] > arr[j + 1]) { 
-                    // swap temp and arr[i] 
+                    // swap 
                     int temp = arr[j]; 
                     arr[j] = arr[j + 1]; 
                     arr[j + 1] = temp; 
@@ -15,11 +15,15 @@ class BubbleSort {
     public static void main(String args[]) 
     { 
         BubbleSort ob = new BubbleSort(); 
+        // array to store numbers
         int arr[] = { 64, 34, 25, 12, 22, 11, 90 }; 
         ob.bubbleSort(arr);
-        int small = arr[0];
-        int secondsmall = arr[1];
-        System.out.println("Smallest number in the array "+"->"+small);
-        System.out.println("Second smallest number in the array "+"->"+secondsmall);
+        // to store smallest number
+        int small_number = arr[0];
+        // to store 2nd smallest number
+        int second_small_number = arr[1];
+        // printing the smallest and 2nd smallest numbers
+        System.out.println("Smallest number in the array "+"->"+small_number);
+        System.out.println("Second smallest number in the array "+"->"+second_small_number);
     } 
 }
