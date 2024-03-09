83256c4fe21dc35134fa477e31cbfd07f8dea12b
cheryl811 <cheryllalparakal@gmail.com>
2024-03-09 13:42:51 +0530

    2nd commit

--- array/array2.java
+++ array/array2.java
@@ -11,17 +11,7 @@ class BubbleSort {
                     arr[j + 1] = temp; 
                 } 
     } 
-  
-    // Prints the array 
-    void printArray(int arr[]) 
-    { 
-        int n = arr.length; 
-        for (int i = 0; i < n; ++i) 
-            System.out.print(arr[i] + " "); 
-        System.out.println(); 
-    } 
-  
-    // Driver method to test above 
+ 
     public static void main(String args[]) 
     { 
         BubbleSort ob = new BubbleSort(); 
